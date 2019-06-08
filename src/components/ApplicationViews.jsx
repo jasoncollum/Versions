a7f5d10623e04493fb82f60aa7653aa0774c6db1
Jason Collum <jasoncollum@gmail.com>
2019-06-08 17:06:17 -0500

    setup applicationViews

---
+++ src/components/ApplicationViews.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+// import { Route, Redirect } from 'react-router-dom';
+
+class ApplicationViews extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Under Construction</h1>
+            </div>
+        )
+    }
+}
+
+export default ApplicationViews
