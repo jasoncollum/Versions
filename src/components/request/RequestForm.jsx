1034e54fff6996b97ae54882b7f9499efee8ca10
Jason Collum <jasoncollum@gmail.com>
2019-06-10 16:38:40 -0500

    add fully dynamic RequestForm

--- src/components/request/RequestForm.jsx
+++ src/components/request/RequestForm.jsx
@@ -7,28 +7,55 @@ export default class RequestForm extends Component {
         songTitleInput: '',
         versionNumberInput: '',
         artistNameInput: '',
-        requestInputs: []
+        requests: [{ text: '' }]
     }
 
-    handleFieldChange = evt => {
-        const stateToChange = {}
-        stateToChange[evt.target.id] = evt.target.value
-        this.setState(stateToChange)
+    addRequest = (e) => {
+        this.setState((prevState) => ({
+            requests: [...prevState.requests, { text: '' }]
+        }));
     }
 
-    addRequestInput = () => {
-        const requestInputs = document.querySelectorAll('#requestGroup textarea')
-        requestInputs.forEach(input => {
-            let floatState = this.state.requestInputs
-            floatState.push(input.value)
-            this.setState({ requestInputs: floatState })
-        })
+    handleSubmit = (e) => { e.preventDefault() }
+
+    handleFieldChange = e => {
+        if (['text'].includes(e.target.className)) {
+            let requests = [...this.state.requests]
+            requests[e.target.dataset.id][e.target.className] = e.target.value
+            this.setState({ requests }, () => console.log(this.state.requests))
+        } else {
+            this.setState({ [e.target.name]: e.target.value })
+        }
+
+        // const stateToChange = {}
+        // stateToChange[evt.target.id] = evt.target.value
+        // this.setState(stateToChange)
     }
+
+    // Dynamically add request inputs
+    // addRequestInput = (parent) => {
+    //     const newRequestTextarea = `<Input type="textarea" name="requestInput" className="requestInput"
+    // placeholder = "Enter a mix request..." />`
+
+    //     parent.append(newRequestTextarea)
+    // }
+
+    // move to submit button
+    // pushRequests = () => {
+    //     const requestInputs = document.querySelectorAll('#requestGroup textarea')
+    //     requestInputs.forEach(input => {
+    //         let floatState = this.state.requestInputs
+    //         floatState.push(input.value)
+    //         this.setState({ requestInputs: floatState })
+    //     })
+    // }
 
     render() {
+        let { requests } = this.state
         console.log(this.state)
+
         return (
-            <Form id="requestForm">
+            <Form onSubmit={this.handleSubmit} id="requestForm">
                 <Row form>
                     <Col md={6}>
                         <FormGroup>
@@ -55,14 +82,30 @@ export default class RequestForm extends Component {
                         </FormGroup>
                     </Col>
                 </Row>
+                <hr></hr>
                 <Row form>
                     <Col md={12}>
                         <FormGroup id="requestGroup">
-                            <Label for="requestInput">Mix Requests</Label>
-                            <Input type="textarea" name="requestInput" id="requestInput"
-                                placeholder="Enter a mix request..." />
+                            <p>Mix Requests</p>
+                            {
+                                requests.map((val, idx) => {
+                                    let requestId = `request-${idx}`
+                                    return (
+                                        <div key={idx}>
+                                            {/* <Label for={requestId}>Mix Requests</Label> */}
+                                            <Input
+                                                type="textarea"
+                                                name={requestId}
+                                                data-id={idx}
+                                                id={requestId}
+                                                placeholder="Enter a mix request..."
+                                                onChange={this.handleFieldChange} />
+                                        </div>
+                                    )
+                                })
+                            }
+                            <Button onClick={this.addRequest} id="requestBtn">+</Button>
                         </FormGroup>
-                        <Button onClick={this.addRequestInput} id="requestBtn">+</Button>
                     </Col>
                 </Row>
                 <Button>Submit</Button>
