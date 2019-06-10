bc633137150865fd38ab4f13f578690cb1d8c3e6
Jason Collum <jasoncollum@gmail.com>
2019-06-10 13:43:50 -0500

    add handleChange to set state of requestForm

--- src/components/request/RequestForm.jsx
+++ src/components/request/RequestForm.jsx
@@ -3,8 +3,21 @@ import { Col, Row, Button, Form, FormGroup, Label, Input, FormText } from 'react
 import './requestForm.css'
 
 export default class RequestForm extends Component {
+    state = {
+        songTitleInput: '',
+        versionNumberInput: '',
+        artistNameInput: '',
+        requestInput: ''
+    }
+
+    handleFieldChange = evt => {
+        const stateToChange = {}
+        stateToChange[evt.target.id] = evt.target.value
+        this.setState(stateToChange)
+    }
 
     render() {
+        console.log(this.state)
         return (
             <Form id="requestForm">
                 <Row form>
@@ -12,32 +25,36 @@ export default class RequestForm extends Component {
                         <FormGroup>
                             <Label for="songTitleInput">Song Title</Label>
                             <Input type="text" name="songTitleInput" id="songTitleInput"
-                                placeholder="Enter a song title" />
+                                placeholder="Enter a song title"
+                                onChange={this.handleFieldChange} />
                         </FormGroup>
                     </Col>
                     <Col md={2}>
                         <FormGroup>
                             <Label for="versionNumberInput">Version No.</Label>
                             <Input type="text" name="versionNumberInput" id="versionNumberInput"
-                                placeholder="1" />
+                                placeholder="1"
+                                onChange={this.handleFieldChange} />
                         </FormGroup>
                     </Col>
                     <Col md={4}>
                         <FormGroup>
                             <Label for="artistNameInput">Artist Name</Label>
                             <Input type="text" name="artistNameInput" id="artistNameInput"
-                                placeholder="Enter artist name" />
+                                placeholder="Enter artist name"
+                                onChange={this.handleFieldChange} />
                         </FormGroup>
                     </Col>
                 </Row>
                 <Row form>
                     <Col md={12}>
-                        <FormGroup>
-                            <Label for="mixRequestTextarea">Mix Requests</Label>
-                            <Input type="textarea" name="mixRequestTextarea" id="mixRequestTextarea"
-                                placeholder="Enter a mix request..." />
+                        <FormGroup id="requestGroup">
+                            <Label for="requestInput">Mix Requests</Label>
+                            <Input type="textarea" name="requestInput" id="requestInput"
+                                placeholder="Enter a mix request..."
+                                onChange={this.handleFieldChange} />
                         </FormGroup>
-                        <Button>+</Button>
+                        <Button id="requestBtn">+</Button>
                     </Col>
                 </Row>
                 <Button>Submit</Button>
