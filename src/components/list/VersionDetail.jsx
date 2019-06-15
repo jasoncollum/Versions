f3729c5d78249cde01a9e462d6075f8120428b6c
Jason Collum <jasoncollum@gmail.com>
2019-06-15 16:57:27 -0500

    add generic modal

--- src/components/list/VersionDetail.jsx
+++ src/components/list/VersionDetail.jsx
@@ -1,10 +1,22 @@
 import React, { Component } from 'react'
+// import EditFormModal from '../Modal/EditFormModal'
+import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap'
 
+import 'bootstrap/dist/css/bootstrap.css';
 
 export default class VersionDetail extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            modal: false
+        };
 
-    handleEditBtn = () => {
-        console.log('edit version')
+        this.toggle = this.toggle.bind(this);
+    }
+
+    toggle() {
+        console.log('toggled')
+        this.setState(prevState => ({ modal: !prevState.modal }))
     }
 
     handleDeleteBtn = () => {
@@ -14,7 +26,6 @@ export default class VersionDetail extends Component {
 
     render() {
         if (this.props.version.song) {
-            console.log(this.props.version)
             return (
                 <section className="versionDetail">
                     <div className="card-body">
@@ -28,8 +39,18 @@ export default class VersionDetail extends Component {
                                 )
                             }
                         </div>
-                        <button onClick={this.handleEditBtn} className="">Edit Version</button>
+                        <Button onClick={this.toggle}>Edit Version</Button>
                         <button onClick={this.handleDeleteBtn} className="">X</button>
+                        <Modal isOpen={this.state.modal} toggle={this.toggle} className={this.props.className}>
+                            <ModalHeader toggle={this.toggle}>Modal title</ModalHeader>
+                            <ModalBody>
+                                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                            </ModalBody>
+                            <ModalFooter>
+                                <Button color="primary" onClick={this.toggle}>Save Changes</Button>{' '}
+                                <Button color="secondary" onClick={this.toggle}>Cancel</Button>
+                            </ModalFooter>
+                        </Modal>
                     </div>
                 </section>
             )
